08a0be7ffee3cd93f29be16e19dd7736bd8cdfe5
opilane <opilane@HP-CZC2349HV2>
2023-04-18 15:28:02 +0300

    Add project files.

---
+++ vadim_animeeremine.py
@@ -0,0 +1,34 @@
+﻿import pygame, sys
+pygame.init()
+#varvid
+kollane=[255,255,10]
+punane=[255,0,0]
+hall=[200,200,200]
+roosa=[255,150,255]
+roheline=[100,255,100]
+
+#ekraani suurus
+X=640
+Y=480
+ekraan=pygame.display.set_mode([X,Y])
+pygame.display.set_caption("Animatsion")
+ekraan.fill(roheline)
+kell=pygame.time.Clock()
+mesilane=pygame.image.load("biig.png")
+posX=X-mesilane.get_rect().width
+posY=Y-mesilane.get_rect().height
+lõpp=False
+samm=2
+while not lõpp:
+    kell.tick(60)
+    events=pygame.event.get()
+    for i in pygame.event.get():
+        if i.type==pygame.QUIT():
+            sys.exit()
+    ekraan.blit(mesilane,(posX,posY))
+    posX-=samm
+    if posX>X-mesilane.get_rect().width or posX<0:
+        samm=-samm
+    pygame.display.flip()
+    ekraan.fill(roheline)
+pygame.quit()
